ee5ef8caed063ae64e84f409360ba3ac19f50e1e
ryosuke horii <horii4153213@gmail.com>
2016-07-11 09:59:23 +0900

    mission clear!

--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,7 @@
+
 //resource.js
 var res = {
-  cover_png : "res/cover.png",
+    cover_png : "res/cover.png",
     tile_0_png : "res/tile_0.png",
     tile_1_png : "res/tile_1.png",
     tile_2_png : "res/tile_2.png",
@@ -9,7 +10,17 @@ var res = {
     tile_5_png : "res/tile_5.png",
     tile_6_png : "res/tile_6.png",
     tile_7_png : "res/tile_7.png",
-    
+    tile_8_png : "res/tile_8.png",
+    tile_9_png : "res/tile_9.png",
+    tile_10_png : "res/tile_10.png",
+    tile_11_png : "res/tile_11.png",
+    tile_12_png : "res/tile_12.png",
+    tile_13_png : "res/tile_13.png",
+    tile_14_png : "res/tile_14.png",
+    tile_15_png : "res/tile_15.png",
+    tile_16_png : "res/tile_16.png",
+    tile_17_png : "res/tile_17.png",
+
 };
 
 var g_resources = [];
